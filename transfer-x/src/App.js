050f02a31c292dd60310d38369c5ca24497c12fa
Thein Htet Aung <theinhtet.aung@silabs.com>
2023-03-24 20:07:28 +0800

    Log-in feature update
    Implement the front end part for the log in feature:
    - Add background image
    - Add username and password fields
    - Add autofocus for the username
    - Center-align the Log In button

---
+++ transfer-x/src/App.js
@@ -0,0 +1,16 @@
+import Login from "./components/login";
+
+function App() {
+  return (
+    <>
+      <div className="appContainer">
+        <Login />;
+        <div className="backgroundcover-text">
+          <a href="https://www.freepik.com/free-vector/abstract-blue-geometric-shapes-background_6166980.htm#query=background&position=4&from_view=keyword&track=sph">Image by Harryarts</a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
