18d97810eedcc072541d8ec9ed7f8139c9767274
superblackblood97 <t.htetaung.9221@gmail.com>
2023-03-27 20:21:14 +0800

    Login logic restructuring and css modification for background image

--- transfer-x/src/App.js
+++ transfer-x/src/App.js
@@ -1,16 +1,17 @@
 import Login from "./components/login";
+import Transfer from "./components/transfer";
+import { connect } from "react-redux";
 
-function App() {
+
+function App({ isValidUser }) {
   return (
-    <>
-      <div className="appContainer">
-        <Login />;
-        <div className="backgroundcover-text">
-          <a href="https://www.freepik.com/free-vector/abstract-blue-geometric-shapes-background_6166980.htm#query=background&position=4&from_view=keyword&track=sph">Image by Harryarts</a>
-        </div>
+    <div className="appContainer">
+      {isValidUser ? <Transfer /> : <Login />}
+      <div className="backgroundcover-text">
+        <a href="https://www.freepik.com/free-vector/abstract-blue-geometric-shapes-background_6166980.htm#query=background&position=4&from_view=keyword&track=sph">Image by Harryarts</a>
       </div>
-    </>
+    </div>
   );
 }
 
-export default App;
+export default connect((state) => ({ isValidUser: state.isValidUser }))(App);
