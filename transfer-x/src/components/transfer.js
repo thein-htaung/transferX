979bc91a22a901232d88c7220f6a7638fc430ae7
superblackblood97 <t.htetaung.9221@gmail.com>
2023-04-02 20:50:23 +0800

    Add firebase and firebase storage logic for upload button

--- transfer-x/src/components/transfer.js
+++ transfer-x/src/components/transfer.js
@@ -1,30 +1,50 @@
 import { Button, Card, Grid, Icon } from "semantic-ui-react";
-import styled from "styled-components";
+import { initializeApp } from "firebase/app";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  uploadBytesResumable,
+} from "firebase/storage";
+
 import { useState } from "react";
+import storage from "../firebaseConfig";
 
-const StyledCenter = styled.div`
-  text-align: center;
-`;
 const Transfer = () => {
-    const [isUpload, setIsUpload] = useState(false);
-    const [isDownload, setIsDownload] = useState(false);
+  const [isUpload, setIsUpload] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [isDownload, setIsDownload] = useState(false);
+  const [uploadProgress, setUploadProgress] = useState(0);
 
-  const uploadHandler = () => {
-    setIsUpload((prevState) => (!prevState));
+  const handleFileSelect = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+  const uploadHandler = (uploadFile) => {
+    setIsUpload((prevState) => !prevState);
+    console.log(uploadFile.name);
+    const storageRef = ref(storage, uploadFile.name);
+    const uploadTask = uploadBytesResumable(storageRef, uploadFile);
+
+    uploadTask.on("state_changed", (snapshot) => {
+      // Observe state change events such as progress, pause, and resume
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setUploadProgress(progress);
+    });
   };
 
   const downloadHandler = () => {
-    setIsDownload((prevState) => (!prevState));
+    setIsDownload((prevState) => !prevState);
   };
 
   return (
     <div className="center-card">
       <Card
         style={{
-          width: '500px',
-          paddingLeft: '40px',
-          paddingRight: '40px',
-          borderRadius: '15px',
+          width: "500px",
+          paddingLeft: "40px",
+          paddingRight: "40px",
+          borderRadius: "15px",
         }}
       >
         <Card.Content>
@@ -45,11 +65,32 @@ const Transfer = () => {
                     alignItems: "center",
                   }}
                 >
+                  <input type="file" onChange={handleFileSelect} />
                   <Icon.Group size="huge">
-                    {isUpload && <Icon loading size="big" color="teal" name="circle notch" />}
-                    <Icon color="blue" name="arrow alternate circle up" onClick={uploadHandler}/>
+                    {isUpload && uploadProgress !== 100 && (
+                      <Icon
+                        loading
+                        size="big"
+                        color="teal"
+                        name="circle notch"
+                      />
+                    )}
+                    {
+                      <Icon
+                        color="blue"
+                        name="arrow alternate circle up"
+                        disabled={!selectedFile}
+                        onClick={() => uploadHandler(selectedFile)}
+                      />
+                    }
                   </Icon.Group>
                   <span>upload</span>
+                  {uploadProgress > 0 && (
+                    <div>
+                      <progress value={uploadProgress} max="100" />
+                      <span>{uploadProgress.toFixed(2)}% uploaded</span>
+                    </div>
+                  )}
                 </div>
               </Grid.Column>
               <Grid.Column width={8} />
@@ -62,8 +103,19 @@ const Transfer = () => {
                   }}
                 >
                   <Icon.Group size="huge">
-                    {isDownload && <Icon loading size="big" color="teal" name="circle notch" />}
-                    <Icon color="green" name="arrow alternate circle down" onClick={downloadHandler}/>
+                    {isDownload && (
+                      <Icon
+                        loading
+                        size="big"
+                        color="teal"
+                        name="circle notch"
+                      />
+                    )}
+                    <Icon
+                      color="green"
+                      name="arrow alternate circle down"
+                      onClick={downloadHandler}
+                    />
                   </Icon.Group>
                   <span>download</span>
                 </div>
